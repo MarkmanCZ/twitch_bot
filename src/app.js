f1ea535ff355227633210e986fe34b228cbffdd9
MarkmanCZ <78849760+MarkmanCZ@users.noreply.github.com>
2021-09-24 12:43:26 +0200

    Pridan funkcni bot

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import tmi from 'tmi.js'
-import { BOT_USERNAME, OAUTH_TOKEN, CHANNEL_NAME } from './constants';
+import { BOT_USERNAME, OAUTH_TOKEN, CHANNEL_NAME, BLOCKED_WORDS } from './constants'
 
 const options = {
     options: { debug: true },
@@ -21,8 +21,29 @@ client.connect();
 
 client.on('message', (channel, userstate, message, self) => {
 	if(self) return;
+
+	if (userstate.username === BOT_USERNAME) return;
+
+
 	if(message.toLowerCase() === '!ahoj') {
 		client.say(channel, `@${userstate.username}, nazdar!`);
 	}
+
+	checkTwitchChatt (userstate, message, channel);
+	
 });
 		
+
+function checkTwitchChatt(userstate, message, channel) {
+	message = message.toLocaleLowerCase() 
+	let shouldSendMessage = false;
+	//zkontroluj zpravu
+	shouldSendMessage = BLOCKED_WORDS.some(blockedWord => message.includes(blockedWord.toLocaleLowerCase()))
+		
+	if (shouldSendMessage) {
+		//varuj uzivatele
+		client.say(channel, `@${userstate.username},omluváme se! Vaše zpráva byla smazaná.`)
+		//smaz zpravu
+
+	}
+}
